74440bd172eaad83e5d83bac638def84e83ed9d3
joaachua <ajoannachua@gmail.com>
2025-05-01 18:04:17 +0800

    include swagger and cors

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+
+// CORS
+const cors = require("cors");
+
+// Routes
 const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/user");
+
+// Swagger
+const { swaggerUi, specs } = require("./config/swagger");
 
 require("dotenv").config();
 
 app.use(express.json());
+
+app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
