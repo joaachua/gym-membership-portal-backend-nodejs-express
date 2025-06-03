944084422f075b8c1332ee66d348909f111a608a
joaachua <ajoannachua@gmail.com>
2025-06-03 21:40:58 +0800

    use the correct variable

--- routes/admin.js
+++ routes/admin.js
@@ -13,21 +13,21 @@ router.post('/forgot-password', AuthController.forgotPassword);
 router.post('/reset-password', AuthController.resetPassword);
 
 router.use(authenticateToken);
-router.post("/ads/upload-file", authorize(['Ads Management-Create']), upload.single('file'), AdsController.uploadFile);
+router.post("/ads/upload-file", authorize(['Ads Management-Create']), uploadImage.single('file'), AdsController.uploadFile);
 router.post("/ads/store", authorize(['Ads Management-Create']), AdsController.storeAds);
 router.post("/ads/update", authorize(['Ads Management-Edit']), AdsController.updateAds);
 router.post("/ads/view", authorize(['Ads Management-View']), AdsController.viewAds);
 router.post("/ads/delete", authorize(['Ads Management-Delete']), AdsController.deleteAds);
 router.post("/ads/list", authorize(['Ads Management-List']), AdsController.adsList);
 
-router.post("/class/upload-file", authorize(['Class Management-Create']), upload.single('file'), ClassController.uploadFile);
+router.post("/class/upload-file", authorize(['Class Management-Create']), uploadImage.single('file'), ClassController.uploadFile);
 router.post("/class/store", authorize(['Class Management-Create']), ClassController.storeClass);
 router.post("/class/update", authorize(['Class Management-Edit']), ClassController.updateClass);
 router.post("/class/view", authorize(['Class Management-View']), ClassController.viewClass);
 router.post("/class/delete", authorize(['Class Management-Delete']), ClassController.deleteClass);
 router.post("/class/list", authorize(['Class Management-List']), ClassController.classList);
 
-router.post("/centre/upload-file", authorize(['Centre Management-Create']), upload.single('file'), CentreController.uploadFile);
+router.post("/centre/upload-file", authorize(['Centre Management-Create']), uploadImage.single('file'), CentreController.uploadFile);
 router.post("/centre/store", authorize(['Centre Management-Create']), CentreController.storeCentre);
 router.post("/centre/update", authorize(['Centre Management-Edit']), CentreController.updateCentre);
 router.post("/centre/view", authorize(['Centre Management-View']), CentreController.viewCentre);
